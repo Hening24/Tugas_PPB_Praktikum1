acede693055c86e308f7b418e7ab57a70d33f14e
hening <kirigayamanda@gmail.com>
2026-09-09 22:02:33 +0700

    perbaikan index

--- src/Index.js
+++ src/Index.js
@@ -20,4 +20,9 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});app.get("/", (req, res) => {
+  res.json({ message: "Sales API is running" });
 });
+
+
+export default app;
